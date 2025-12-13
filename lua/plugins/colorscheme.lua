4daff85cb0e110801490be381cd7871573a47ae5
piti83 <106607310+piti83@users.noreply.github.com>
2025-12-13 16:51:51 +0100

    Switching color theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,16 +1,14 @@
 return {
-	
-  { 
-    "ellisonleao/gruvbox.nvim", 
-    priority = 1000, 
-    config = function()
-    require("gruvbox").setup({
-      transparent_mode = true,
-    })
-    vim.cmd.colorscheme "gruvbox"
-  end,
-  },
-
+    { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+        config = function()
+            require("catppuccin").setup({
+                transparent_background = true,
+            })
+            vim.cmd.colorscheme "catppuccin"
+        end
+    }
 }
-
-
