01ca516c744885e5657fe77e68ecde9196245fca
piti83 <106607310+piti83@users.noreply.github.com>
2026-09-05 16:37:36 +0200

    clangd fix

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,12 +1,72 @@
 return {
-    { "williamboman/mason.nvim", config = true },
-    { "neovim/nvim-lspconfig" },
     {
-        "williamboman/mason-lspconfig.nvim",
-        dependencies = { "williamboman/mason.nvim", "neovim/nvim-lspconfig" },
-        config = function()
-            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+        "mason-org/mason.nvim",
+        opts = {},
+    },
 
+    {
+        "neovim/nvim-lspconfig",
+    },
+
+    {
+        "mason-org/mason-lspconfig.nvim",
+        dependencies = {
+            "mason-org/mason.nvim",
+            "neovim/nvim-lspconfig",
+        },
+
+        config = function()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+            local servers = {
+                "lua_ls",
+                "asm_lsp",
+                "jsonls",
+                "bashls",
+                "jdtls",
+            }
+
+            for _, server in ipairs(servers) do
+                vim.lsp.config(server, {
+                    capabilities = capabilities,
+                })
+            end
+
+            vim.lsp.config("pyright", {
+                capabilities = capabilities,
+                settings = {
+                    python = {
+                        analysis = {
+                            typeCheckingMode = "off",
+                            diagnosticSeverityOverrides = {
+                                reportGeneralTypeIssues = "none",
+                                reportOptionalMemberAccess = "none",
+                            },
+                        },
+                    },
+                },
+            })
+
+            local clangd_cmd = {
+                "clangd",
+                "--background-index",
+            }
+
+            local cxx = vim.env.CXX
+
+            if cxx and cxx ~= "" then
+                local compiler = vim.split(cxx, "%s+")[1]
+                local driver = vim.fn.exepath(compiler)
+
+                if driver ~= "" then
+                    table.insert(clangd_cmd, "--query-driver=" .. driver)
+                end
+            end
+
+            vim.lsp.config("clangd", {
+                capabilities = capabilities,
+                cmd = clangd_cmd,
+            })
 
             require("mason-lspconfig").setup({
                 ensure_installed = {
@@ -16,41 +76,7 @@ return {
                     "jsonls",
                     "pyright",
                     "bashls",
-                    "jdtls"
-                },
-                handlers = {
-                    function(server_name)
-                        require("lspconfig")[server_name].setup({
-                            capabilities = capabilities,
-                            on_attach = on_attach,
-                        })
-                    end,
-                    ["pyright"] = function()
-                        require("lspconfig").pyright.setup({
-                            capabilities = capabilities,
-                            settings = {
-                                python = {
-                                    analysis = {
-                                        typeCheckingMode = "off",
-                                        diagnosticSeverityOverrides = {
-                                            reportGeneralTypeIssues = "none",
-                                            reportOptionalMemberAccess = "none",
-                                        },
-                                    },
-                                },
-                            },
-                        })
-                    end,
-                    ["clangd"] = function()
-                        require("lspconfig").clangd.setup({
-                            capabilities = capabilities,
-                            cmd = {
-                                "clangd",
-                                "--background-index",
-                                "--query-driver=/**/*gcc,/**/*g++",
-                            },
-                        })
-                    end,
+                    "jdtls",
                 },
             })
         end,
